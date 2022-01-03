5e5c7c816ad52eae377789fd9ef982e6c42ed364
omerffaruk <farukyamak11044@gmail.com>
2022-01-03 14:26:07 +0000

    feat: NavBar component given content with links to other routes/components

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+      <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about" >About</Link>
+          <Link to="/projects" >Projects</Link>
+          <Link to="/contact" >Contact</Link>
+      </nav>
+    );
+}
+
+export default NavBar;
