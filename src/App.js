60dfef53467af55484e6fbc58c80e0105b56b4e8
omerffaruk <farukyamak11044@gmail.com>
2022-01-03 16:32:39 +0000

    feat: Profile component route created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
+import Profile from "./components/Profile";
 import ErrorPage from "./components/ErrorPage";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/profile/:username" element={<Profile />} />
         <Route path="*" element={<ErrorPage />} /> 
       </Routes>
     </div>
