89da4a58f37457d5d1c88f02671c535ae2c8fac9
omerffaruk <farukyamak11044@gmail.com>
2022-01-03 14:01:51 +0000

    feat: About Route creted

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
