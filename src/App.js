7ab3d1c6b3a51187f5661326870633af6d1630c4
omerffaruk <farukyamak11044@gmail.com>
2022-01-03 16:10:51 +0000

    feat: Error Page route created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<ErrorPage />} /> 
       </Routes>
     </div>
   );
