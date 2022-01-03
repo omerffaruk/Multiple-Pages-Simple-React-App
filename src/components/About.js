b6da82a55151dc06e8e3267578d7fff4e924789a
omerffaruk <farukyamak11044@gmail.com>
2022-01-03 16:20:32 +0000

    feat: navigate to projects component from about component with useNavigate

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function About() {
+    const navigate = useNavigate();
+    const handleNavigateToProjectsButton = () => navigate('/projects');
     return (
         <div>
             <h1>About</h1>
+            <button onClick={handleNavigateToProjectsButton}>Click to see my projects!</button>
         </div>
     )
 }
